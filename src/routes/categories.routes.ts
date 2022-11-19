d672a1b6334649bf5d058114756fb43ca27ac999
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-11-18 23:26:19 -0300

    [ADD] Existing categories listing endpoint

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -4,12 +4,23 @@ import { CategoriesRepository } from '../repositories/CategoriesRepository';
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
+
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists)
+    return response.status(400).json({ error: 'Category already exists.' });
 
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
