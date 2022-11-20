7fc0d854d01d66a5dce2883c8127acbaf5819390
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-11-19 23:07:46 -0300

    [ADD] CreateCategoryService

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,23 @@
+import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  //   private categoriesRepository: CategoriesRepository;
+  constructor(private categoriesRepository: ICategoriesRepository) {
+    // this.categoriesRepository = categoriesRepository;
+  }
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) throw new Error('Category already exists.');
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };
