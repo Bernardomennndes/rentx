0c29132f14ee071d40f566188be29e5d224d2552
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-11-21 22:58:22 -0300

    [REFACTOR] CreateCategory route

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,11 +1,11 @@
-import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-class CreateCategoryService {
+class CreateCategoryUseCase {
   //   private categoriesRepository: CategoriesRepository;
   constructor(private categoriesRepository: ICategoriesRepository) {
     // this.categoriesRepository = categoriesRepository;
@@ -20,4 +20,4 @@ class CreateCategoryService {
   }
 }
 
-export { CreateCategoryService };
+export { CreateCategoryUseCase };
