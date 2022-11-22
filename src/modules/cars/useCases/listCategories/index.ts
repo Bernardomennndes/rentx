69160233e93063c5c56f903333148b23c5823900
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-11-21 23:20:27 -0300

    [ADD] Singleton Pattern added to Categories Repository

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -2,7 +2,7 @@ import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 import { ListCategoriesController } from './ListCategoriesController';
 import { CategoriesRepository } from '../../repositories/CategoriesRepository';
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstance();
 
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
 
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories/';
 
