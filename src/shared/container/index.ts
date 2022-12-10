c22aaa579d27b96057e33bb8f1372e66729011ba
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-12-09 23:24:01 -0300

    [ADD] UserRepostitory singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,11 @@
 import { container } from 'tsyringe';
 
-import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
+import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { SpecificationRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
@@ -13,4 +15,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   'SpecificationRepository',
   SpecificationRepository,
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
 );
