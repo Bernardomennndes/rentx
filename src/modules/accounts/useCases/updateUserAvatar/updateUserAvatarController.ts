517a49a7a7cf0109f7aaf4bff5c0122186b1e9ba
Bernardo Mendes <bernardomennndes@outlook.com>
2022-12-25 16:47:06 -0300

    [ADD] UserAvatar upload route

--- src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarController.ts
@@ -1,12 +1,11 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
-import { UpdateUserAvatarUseCase } from './updateUserAvatarUseCase';
+import { UpdateUserAvatarUseCase } from './UpdateUserAvatarUseCase';
 
 class UpdateUserAvatarController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.user;
-
-    const avatar_file = null;
+    const avatar_file = request.file.filename;
 
     const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 
