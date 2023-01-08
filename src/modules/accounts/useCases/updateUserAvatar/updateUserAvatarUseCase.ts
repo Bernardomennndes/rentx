c8418e59fc8f34e284b0b16d4f9219e56fd7a6bc
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-08 18:47:47 -0300

    [REFACTOR] @utils import path

--- src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
-import { deleteFile } from 'utils/file';
+import { deleteFile } from '@utils/file';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 
 interface IRequest {
