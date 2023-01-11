e8e3cb61a1284ca961b921ed77f828a327306dba
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-10 23:18:49 -0300

    [FIX] Import paths fixed

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,5 +1,5 @@
-import { User } from "../entities/User";
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { User } from "../infra/typeorm/entities/User";
 
 
 interface IUsersRepository {
