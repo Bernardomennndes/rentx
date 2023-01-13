69de35e4be450ec72a3f7dfec7a754a5f3d92352
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-12 23:45:44 -0300

    [ADD] CarsRepositoryInMemory

---
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -0,0 +1,34 @@
+import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+
+import { ICarsRepository } from '../ICarsRepository';
+
+class CarsRepositoryInMemory implements ICarsRepository {
+  cars: Car[];
+
+  async create({
+    name,
+    description,
+    brand,
+    category_id,
+    daily_rate,
+    fine_amount,
+    license_place,
+  }: ICreateCarDTO): Promise<void> {
+    const car = new Car();
+
+    Object.assign(car, {
+      name,
+      description,
+      brand,
+      category_id,
+      daily_rate,
+      fine_amount,
+      license_place,
+    });
+
+    this.cars.push(car);
+  }
+}
+
+export { CarsRepositoryInMemory };
