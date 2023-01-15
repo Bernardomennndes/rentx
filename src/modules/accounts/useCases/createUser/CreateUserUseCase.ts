e401a110d17ae208513b07ea795b769721a97ba8
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-15 18:30:25 -0300

    [FIX] bcryptjs lib applied

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'tsyringe';
-import { hash } from 'bcrypt';
+import { hash } from 'bcryptjs';
 
 import { AppError } from '@shared/errors/AppError';
 import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
