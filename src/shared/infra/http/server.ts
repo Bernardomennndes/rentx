7d5819b29ee3c7337ad47c78984e88b7b6cc2ae7
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-15 18:31:13 -0300

    [FIX] Connection config altered

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,15 +1,16 @@
+import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import '../../../database';
-
 import '@shared/container';
-
-import { router } from './routes';
-import swaggerFile from '../../../swagger.json';
 import { AppError } from '@shared/errors/AppError';
+import createConnection from '@shared/infra/typeorm';
 
+import swaggerFile from '../../../swagger.json';
+import { router } from './routes';
+
+createConnection();
 const app = express();
 
 app.use(express.json());
@@ -19,15 +20,17 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError)
-      return response.status(err.statusCode).json({ message: err.message });
+	(err: Error, request: Request, response: Response, next: NextFunction) => {
+		if (err instanceof AppError)
+			return response
+				.status(err.statusCode)
+				.json({ message: err.message });
 
-    return response.status(500).json({
-      status: 'error',
-      message: `Internal server error - ${err.message}`,
-    });
-  },
+		return response.status(500).json({
+			status: 'error',
+			message: `Internal server error - ${err.message}`,
+		});
+	},
 );
 
 app.listen(3333, () => console.log('Server is running.'));
