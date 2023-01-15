e040521488976f5ebe633fe5de57fb320f6d6167
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-14 22:05:43 -0300

    [ADD] Cars routes

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,10 +3,12 @@ import { container } from 'tsyringe';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
@@ -22,3 +24,5 @@ container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
 );
+
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
