ee83d4f5ddd2a034965b0fa533dcc9968f678464
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-20 15:05:12 -0300

    [ADD] UploadCarImages

---
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { UploadCarImagesUseCase } from './UploadCarImagesUseCase';
+
+interface IFiles {
+	filename: string;
+}
+
+class UploadCarImagesController {
+	async handle(request: Request, response: Response): Promise<Response> {
+		const { id } = request.params;
+		const images = request.files as IFiles[];
+
+		const uploadCarImageUseCase = container.resolve(UploadCarImagesUseCase);
+
+		const fileNames = images.map(image => image.filename);
+
+		await uploadCarImageUseCase.execute({
+			car_id: id,
+			images_name: fileNames,
+		});
+
+		return response.status(201).send();
+	}
+}
+
+export { UploadCarImagesController };
