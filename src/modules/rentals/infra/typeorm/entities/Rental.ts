32bbc5f0021c3bf5e36834b6195025d33bbcff12
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-25 20:27:22 -0300

    [ADD] Rentals repository

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,29 +1,44 @@
+import {
+	CreateDateColumn,
+	Entity,
+	PrimaryColumn,
+	UpdateDateColumn,
+	Column,
+} from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+@Entity('rentals')
 class Rental {
+	@PrimaryColumn()
 	id: string;
 
+	@Column()
 	car_id: string;
 
+	@Column()
 	user_id: string;
 
+	@Column()
 	start_date: Date;
 
+	@Column()
 	end_date: Date;
 
+	@Column()
 	expected_return_date: Date;
 
+	@Column()
 	total: number;
 
+	@CreateDateColumn()
 	created_at: Date;
 
+	@UpdateDateColumn()
 	updated_at: Date;
 
 	constructor() {
 		if (!this.id) {
 			this.id = uuidV4();
-			this.created_at = new Date();
-			this.updated_at = new Date();
 		}
 	}
 }
