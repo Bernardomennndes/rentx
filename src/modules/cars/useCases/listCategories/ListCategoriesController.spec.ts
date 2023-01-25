fbe37374f16801894c50f874653fdc783bf479f6
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-25 20:26:47 -0300

    [ADD] ListCategoriesController supertest

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -0,0 +1,76 @@
+import { hash } from 'bcryptjs';
+import request from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
+
+describe('List Categories Controller', () => {
+	beforeAll(async () => {
+		connection = await createConnection();
+		await connection.runMigrations();
+
+		const id = uuidV4();
+		const password = await hash('admin', 8);
+
+		await connection.query(`
+		INSERT INTO USERS (id, name, email, password, "isAdmin", created_at, driver_license) 
+		VALUES ('${id}', 'admin', 'admin@rents.com.br', '${password}', true, 'now()', 'XXXXXX')
+		`);
+	});
+
+	afterAll(async () => {
+		await connection.dropDatabase();
+		await connection.close();
+	});
+
+	it('Should be able to list all categories', async () => {
+		const responseToken = await request(app).post('/sessions').send({
+			email: 'admin@rents.com.br',
+			password: 'admin',
+		});
+
+		const { token } = responseToken.body;
+
+		['Category 1', 'Category 2', 'Category 3'].forEach(async category => {
+			await request(app)
+				.post('/categories')
+				.send({
+					name: category,
+					description: 'Category Supertest Description',
+				})
+				.set({
+					Authorization: `Bearer ${token}`,
+				});
+		});
+
+		const response = await request(app).get('/categories');
+
+		expect(response.statusCode).toBe(200);
+		expect(response.body.length).toBe(3);
+	});
+
+	it('Should not be able to create a new category if name already existing', async () => {
+		const responseToken = await request(app).post('/sessions').send({
+			email: 'admin@rents.com.br',
+			password: 'admin',
+		});
+
+		const { token } = responseToken.body;
+
+		const response = await request(app)
+			.post('/categories')
+			.send({
+				name: 'Category Supertest',
+				description: 'Category Supertest Description',
+			})
+			.set({
+				Authorization: `Bearer ${token}`,
+			});
+
+		expect(response.statusCode).toBe(400);
+	});
+});
