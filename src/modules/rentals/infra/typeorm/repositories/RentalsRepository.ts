50ea77500b17ea5b83d62b7d58c51c99acb5c1fd
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-26 18:20:11 -0300

    [FIX] Add optional missing fields & find if rental is open

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -13,25 +13,37 @@ class RentalsRepository implements IRentalsRepository {
 	}
 
 	async findOpenRentalByCarId(car_id: string): Promise<Rental> {
-		return this.repository.findOne({ car_id });
+		return this.repository.findOne({ where: { car_id, end_date: null } });
 	}
 
 	async findOpenRentalByUserId(user_id: string): Promise<Rental> {
-		return this.repository.findOne({ user_id });
+		return this.repository.findOne({ where: { user_id, end_date: null } });
 	}
 
 	async create({
+		id,
 		car_id,
 		user_id,
 		expected_return_date,
+		end_date,
+		total,
 	}: ICreateRentalDTO): Promise<Rental> {
 		const rental = this.repository.create({
+			id,
 			car_id,
 			user_id,
 			expected_return_date,
+			end_date,
+			total,
 		});
 
 		await this.repository.save(rental);
+
+		return rental;
+	}
+
+	async findById(id: string): Promise<Rental> {
+		const rental = this.repository.findOne({ id });
 
 		return rental;
 	}
