67a5b6939c24502a4de7f103bd05c1e0db851700
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-27 00:28:18 -0300

    [ADD] auth config file

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+	secret_token: 'dada8dd8cf8a81db9786161269f642bc',
+	secret_refresh_token: '721f324e97431c4a03272f00e3e6f132',
+	expires_in_token: '15m',
+	expires_in_refresh_token: '30d',
+	expires_refresh_token_days: 30,
+};
