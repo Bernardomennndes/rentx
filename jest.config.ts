3d7d757e592ae3cec49c4f369efbef402d80c093
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-28 00:05:43 -0300

    [ADD] Config files settings

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,15 @@
-import { compilerOptions } from './tsconfig.json';
 import { pathsToModuleNameMapper } from 'ts-jest';
+
+import { compilerOptions } from './tsconfig.json';
 
 export default {
 	bail: true,
 	clearMocks: true,
+	collectCoverage: true,
+	collectCoverageFrom: ['<rootDir>/src/modules/**/useCases/**/*.ts'],
+	coverageDirectory: 'coverage',
 	coverageProvider: 'v8',
+	coverageReporters: ['text-summary', 'lcov'],
 	preset: 'ts-jest',
 	testMatch: ['**/*.spec.ts'],
 	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
