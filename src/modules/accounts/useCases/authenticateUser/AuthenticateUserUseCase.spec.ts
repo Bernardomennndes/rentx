f081b56e860393e763373e11c00496a8f80828b0
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-28 00:08:31 -0300

    [REFACTOR] Refresh token implemented in files

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,5 +1,7 @@
 import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
 import { UsersRepositoryInMemory } from '@modules/accounts/repositories/in-memory/UsersRepositoryInMemory';
+import { UsersTokensRepositoryInMemory } from '@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory';
+import { DayjsDateProvider } from '@shared/container/providers/DateProvider/implementations/DayjsDateProvider';
 import { AppError } from '@shared/errors/AppError';
 
 import { CreateUserUseCase } from '../createUser/CreateUserUseCase';
@@ -7,13 +9,19 @@ import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
 
 let authenticateUserUseCase: AuthenticateUserUseCase;
 let usersRepositoryInMemory: UsersRepositoryInMemory;
+let usersTokensRepositoryInMemory: UsersTokensRepositoryInMemory;
 let createUserUseCase: CreateUserUseCase;
+let dateProvider: DayjsDateProvider;
 
 describe('Authenticate User', () => {
 	beforeEach(() => {
 		usersRepositoryInMemory = new UsersRepositoryInMemory();
+		dateProvider = new DayjsDateProvider();
+		usersTokensRepositoryInMemory = new UsersTokensRepositoryInMemory();
 		authenticateUserUseCase = new AuthenticateUserUseCase(
 			usersRepositoryInMemory,
+			usersTokensRepositoryInMemory,
+			dateProvider,
 		);
 		createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
 	});
